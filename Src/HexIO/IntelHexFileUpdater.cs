e8d6429ce565a3205f680e7966a1c0894d86491a
alex.elenter <alex.elenter@gitlab.fing.edu.uy>
2022-07-04 10:23:27 -0300

    New feature: load an hex file as a record list + modify it by address + write the new hex hile

---
+++ Src/HexIO/IntelHexFileUpdater.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using HexIO;
+
+namespace MemoryFileManager
+{
+    public class HexFileManager
+    {
+        private FileStream fileStream;
+        private IntelHexReader hexReader;
+        private List<IntelHexRecord> fileByRecord;
+        private string pathToWriteNewHexFile;
+
+        public HexFileManager(string pathToFile, string pathToNewFile)
+        {
+            fileStream = new FileStream(pathToFile, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+            hexReader = new IntelHexReader(fileStream);
+            fileByRecord = TransformHexFileToHexRecordList(hexReader);
+            pathToWriteNewHexFile = pathToNewFile;
+        }
+
+        private List<IntelHexRecord> TransformHexFileToHexRecordList(IntelHexReader hexReader)
+        {
+            List<IntelHexRecord> res = new List<IntelHexRecord>();
+            IntelHexRecord currentRecord;
+            while (hexReader.Read(out currentRecord))
+            {
+                res.Add(currentRecord);
+            }
+            return res;
+        }
+
+        /*
+        * Given dictionnary { address : byte }
+        * Substitutes the byte in the record list 
+        * generated in the constructor
+        */
+        public void WriteByteByAddress(Dictionary<uint, byte> bytesToWrite)
+        {
+            foreach (KeyValuePair<uint, byte> valuePair in bytesToWrite)
+            {
+                WriteByteByAddress(valuePair.Key, valuePair.Value);
+            }
+            WriteNewHexFile();
+        }
+
+        private void WriteByteByAddress(uint address, byte byteToWrite)
+        {
+            //TODO: inefficient cause calculates checksum many times for same line --> one solution: calculate CRC when writing file
+            IntelHexRecord record = fileByRecord.Find(record => record.AddressInHexRecord(address));
+            record.Write(address, byteToWrite);
+            record.CheckSum = CalculateChecksum(record);
+        }
+
+        public static byte CalculateChecksum(IntelHexRecord record)
+        {
+            var addresBytes = BitConverter.GetBytes(record.Address);
+            var hexRecordData = new List<byte>
+            {
+                (byte)record.ByteCount,
+                addresBytes[1],
+                addresBytes[0],
+                (byte)record.RecordType
+            };
+            hexRecordData.AddRange(record.Data);
+            int maskedSumBytes = hexRecordData.Sum(x => x) & 0xFF;
+            byte calculatedChecksum = (byte)(256 - maskedSumBytes);
+            
+            return calculatedChecksum;
+        }
+
+        /*
+        * Writes the new hexfile produced by WriteByAdress method
+        */
+        private void WriteNewHexFile()
+        {
+            int count = 0;
+            using (StreamWriter fileStreamToWrite = new StreamWriter(pathToWriteNewHexFile))
+            {
+                foreach (IntelHexRecord record in fileByRecord)
+                {
+                    fileStreamToWrite.Write(record.GetStringToWrite());
+                    count++;
+                    fileStreamToWrite.Write(Environment.NewLine);
+                }
+            }
+        }
+    }
+}
---
+++ Src/HexIO/Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HexIO;
+using Microsoft.VisualBasic;
+
+namespace MemoryFileManager
+{
+    static class Utility
+    {
+        public static bool AddressInHexRecord(this IntelHexRecord record, uint address)
+        {
+            return (address >= record.Address && address < record.Address + record.ByteCount);
+        }
+
+        public static void Write(this IntelHexRecord record, uint address, byte byteToWrite)
+        {
+            record.Data[(int)(address - record.Address)] = byteToWrite;
+        }
+
+        public static string GetStringToWrite(this IntelHexRecord record)
+        {
+            StringBuilder recordAsString = new StringBuilder();
+            recordAsString.Append(":");
+            recordAsString.Append(record.ByteCount.ToString("X2"));
+            recordAsString.Append(record.Address.ToString("X4"));
+            recordAsString.Append(((byte)record.RecordType).ToString("X2"));
+            recordAsString.Append(ByteArrayToString(record.Data.ToArray()));
+            recordAsString.Append(record.CheckSum.ToString("X2"));
+            return recordAsString.ToString();
+        }
+
+        public static string ByteArrayToString(byte[] byteArray)
+        {
+            StringBuilder res = new StringBuilder();
+            foreach (byte byteToConvert in byteArray)
+            {
+                res.Append(byteToConvert.ToString("X2"));
+            }
+            return res.ToString();
+        }
+    }
+}
